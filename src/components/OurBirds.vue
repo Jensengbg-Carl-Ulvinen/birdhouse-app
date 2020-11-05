642cecdbcb2bafb33800d03d2ae83008f66ba7c1
Jensengbg-Leon-Listo <leon.listo@hotmail.com>
2020-11-05 12:59:16 +0100

    Cards and css update
    added cards and some styling

--- src/components/OurBirds.vue
+++ src/components/OurBirds.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="wrapper">
+    <header class="header">
     <h1>Meet our birds</h1>
     <img id="menu" src="../assets/bars.png">
-    <div class="card-wrapper">
-      <p></p>
-    </div>
+    </header>
+    <ul class="card-wrapper">
+      <li>
+        <img id="macaw" src="../assets/macaw.jpg">
+        Macaws are long-tailed, often colorful parrots. 
+        <br>
+        They are popular in aviculture or as companion parrots.
+        <button class="btn"></button>
+       </li>
+      <li>Bird</li>
+      <li>Bird</li>
+    </ul>
   </div>
 </template>
 
@@ -20,7 +30,7 @@ body{
   margin: 0;
   background-color: $primary-background-colour;
 }
-.wrapper{
+.header{
   background-color: $primary-footer-colour;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
@@ -33,5 +43,33 @@ h1{
   grid-column: 5 / 5;
   margin-left: 10rem;
   margin-top: 1rem;
+}
+ul{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    list-style: none;
+    margin-top: 4rem;
+    margin-right: 2rem;
+}
+li{
+  padding: 3rem;
+  padding-bottom: 8rem;
+  border-radius: 3%;
+  background: $primary-card-colour;
+  width: 145px;
+  height: 120px;
+}
+#macaw{
+    margin-top: -30%;
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+}
+.btn{
+  border-style: none;
+  background-color: $btn-colour;
+  padding: 0.5rem;
+  padding-left: 3rem;
 }
 </style>
