4430c492f2da9cf6dd41a8be5f9c0ec271b7f486
Jensengbg-Carl-Ulvinen <54705232+Jensengbg-Carl-Ulvinen@users.noreply.github.com>
2020-11-05 15:03:30 +0100

    Add assets, style header, style footer, style about

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="birdhouse-logo" src="../assets/birdhouse.png">
+    <img alt="birdhouse-logo" id="birdhouse-logo" src="../assets/birdhouse.png">
     <Welcome msg="Welcome to Birdhouse"/>
   </div>
 </template>
@@ -22,6 +22,30 @@ export default {
 @import '../styles/base.sass';
 
 body {
-  background-color:  $primary-background-colour
+  background-color:  $primary-background-colour;
+
+}
+
+.home{
+  padding-top: 6rem;
+  height: 70vh;
+
+  #birdhouse-logo{
+    height: 300px;
+  }
+}
+
+@media screen and (max-width:736px) {
+
+  .home{
+    padding-top: 6rem;
+    height: 100vh;
+
+    #birdhouse-logo{
+      height: 300px;
+    }
+  }
+
+
 }
 </style>
