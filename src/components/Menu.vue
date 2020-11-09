b3fd5341232f25087a62d5a177dbcacc3a562916
Jensengbg-Carl-Ulvinen <54705232+Jensengbg-Carl-Ulvinen@users.noreply.github.com>
2020-11-09 16:06:19 +0100

    Refactor Menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,49 +1,60 @@
 <template>
-<img id="burger-nav" src="@/assets/burger-menu-desktop.png" v-on:click="onMenu()">
-<div class="overlay-menu" v-on:click="offMenu()">
-  <div class="menu-item">
-    <router-link to="/">Home</router-link>
-  </div>
-  <div class="menu-item">
-    <router-link to="/about">About</router-link>
-  </div>
-  <div class="menu-item">
-    <router-link to="/our-birds">Ourbirds</router-link>
-  </div>
-  <div class="menu-item">
-    <router-link to="/food-drinks">Food and Drinks</router-link>
-  </div>
-</div>
+<transition name="fade">
+  <i class="material-icons menu" v-if="!show" @click="show = !show" key="menu">menu</i>
+  <i class="material-icons clear" v-else onclick="show = !show" key="clear">clear</i>
+</transition>
+
+<transition name="fade">
+  <ul v-if="show">
+    <div class="menu-item">
+      <router-link to="/">Home</router-link>
+    </div>
+    <div class="menu-item">
+      <router-link to="/about">About</router-link>
+    </div>
+    <div class="menu-item">
+      <router-link to="/our-birds">Ourbirds</router-link>
+    </div>
+    <div class="menu-item">
+      <router-link to="/food-drinks">Food and Drinks</router-link>
+    </div>
+  </ul>
+</transition>
+
 </template>
 
 
 <script type="text/javascript">
 export default {
-  name: "Menu",
-  methods: {
-    onMenu: function() {
-      document.getElementById("overlay-menu").style.display = "block";
-    },
-    offMenu: function() {
-      document.getElementById("overlay-menu").style.display = "none";
-    }
-  }
-
+  name: "Menu"
 }
 </script>
 
 <style lang="scss" scoped>
-@import '../styles/base.sass';
-
-#burger-nav {
-    padding-top: 1rem;
-    margin-right: 1rem;
-    height: 30px;
-    cursor: pointer;
-    display: none;
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import '@/styles/base.sass';
+.menu,
+.clear {
+  cursor: pointer;
+  font-size: 3em;
+  padding-left: 5%;
+  transition: all .3s ease;
+  width: 1.5em;
 }
 
-// .overlay-menu {
-//     display: none;
-// }
+.menu {
+  color: $primary-title-colour;
+  &:hover,
+  &:active {
+    color: $btn-colour-hover;
+  }
+}
+
+.clear {
+  color: $btn-colour-hover;
+  &:hover,
+  &:active {
+    color: $primary-title-colour;
+  }
+}
 </style>
