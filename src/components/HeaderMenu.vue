1e0f8ed209ddefc83defd5ef5c2c55cbc05ce4a1
Jensengbg-Carl-Ulvinen <54705232+Jensengbg-Carl-Ulvinen@users.noreply.github.com>
2020-11-09 13:07:42 +0100

    nonfunctional menu overlay

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -2,7 +2,6 @@
 <div id="nav">
   <img id="nav-logo" src="@/assets/header-logo.png">
   <Menu />
-  <img id="burger-nav" src="@/assets/burger-menu-desktop.png">
 </div>
 </template>
 
@@ -22,26 +21,16 @@ export default {
 #nav {
     height: 100px;
     width: 100%;
-    // padding: 1rem;
     background: $primary-header-background;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-}
-
-#burger-nav {
-    padding-top: 1rem;
-    margin-right: 1rem;
-    height: 30px;
-    cursor: pointer;
-
 }
 #nav-logo {
     height: 60px;
     margin-left: 1rem;
     padding-top: 1rem;
 }
-
 a {
     font-weight: bold;
     color: #2c3e50;
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,18 +1,45 @@
 <template>
-<div class="overlay-menu">
-  <div class="menu-item"><router-link to="/">Home</router-link></div>
-  <div class="menu-item"><router-link to="/about">About</router-link></div>
-  <div class="menu-item"><router-link to="/our-birds">Ourbirds</router-link></div>
+<img id="burger-nav" src="@/assets/burger-menu-desktop.png" v-on:click="onMenu()">
+<div class="overlay-menu" v-on:click="offMenu()">
+  <div class="menu-item">
+    <router-link to="/">Home</router-link>
+  </div>
+  <div class="menu-item">
+    <router-link to="/about">About</router-link>
+  </div>
+  <div class="menu-item">
+    <router-link to="/our-birds">Ourbirds</router-link>
+  </div>
 </div>
 </template>
 
-<script>
+
+<script type="text/javascript">
 export default {
-  name: "Menu"
+  name: "Menu",
+  methods: {
+    onMenu: function() {
+      document.getElementById("overlay-menu").style.display = "block";
+    },
+    offMenu: function() {
+      document.getElementById("overlay-menu").style.display = "none";
+    }
+  }
 
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../styles/base.sass';
+
+#burger-nav {
+    padding-top: 1rem;
+    margin-right: 1rem;
+    height: 30px;
+    cursor: pointer;
+}
+
+.overlay-menu {
+    display: none;
+}
 </style>
