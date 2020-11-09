41cda57222b71dab7a623004eac4c904b6fb7355
Jensengbg-Carl-Ulvinen <54705232+Jensengbg-Carl-Ulvinen@users.noreply.github.com>
2020-11-09 11:46:57 +0100

    cleanup App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@ export default {
 </script>
 
 <style lang="scss">
-
 @import '@/styles/base.sass';
 
 #app {
@@ -17,35 +16,10 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
 }
 
 body {
     margin: 0;
-}
-
-#nav {
-    height: auto;
-    //padding: 1rem;
-    background: $primary-header-background;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-
-    #burger-nav {
-        padding-top: 1rem;
-        height: 30px;
-        cursor: pointer;
-    }
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
 }
 
 </style>
